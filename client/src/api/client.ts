b623a5e2cfa42aac66478a44dccca12bdf5786a7
Vivek Kumar <vh118854@gmail.com>
2026-05-28 22:40:54 +0530

    backend url added

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://breath-esg-1.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
